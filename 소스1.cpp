c2d6a1c506f282ade989d475400ab9a34f7c5ee8
hongman321 <goldpig041234@gmail.com>
2024-12-26 17:06:27 +0900

    commit

---
+++ 소스1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+double Avg(double sum) {
+	return sum / 5;
+}
+double sumArray(double* arr, int size) {
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	cout << sum << endl;
+	return sum;
+}
+int main() {
+	double score[5];
+	double sum = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> score[i];
+	}
+	sum = sumArray(score, 5);
+
+	cout << Avg(sum) << endl;
+}
